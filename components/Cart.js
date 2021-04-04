71efecb41ca17a535578caf36ee1bfeb72ada964
ardourApeX <joygupta501@gmail.com>
2021-04-04 17:51:57 +0530

    Feat : Added Cart Prop

---
+++ components/Cart.js
@@ -0,0 +1,57 @@
+import { useCart } from "../contexts/CartContext";
+import { useWishlist } from "../contexts/WishlistContext";
+
+export function Cart() {
+  const [cartState, cartDispatch] = useCart();
+  const [wishlistState, WishlistDispatch] = useWishlist();
+
+  return (
+    <div>
+      {cartState.cartItems.map((itemInCart) => (
+        <div id={itemInCart.id}>
+          <img src={itemInCart.image} alt="img"></img>
+          <h1>{itemInCart.name}</h1>
+          <strong>{itemInCart.price}</strong>
+          <button
+            onClick={() =>
+              cartDispatch({ 
+                query: "SUBTRACT_ONE_MORE", 
+                item: itemInCart })
+            }
+          >
+            -
+          </button>
+          {itemInCart.quantity}
+          <button
+            onClick={() =>
+              cartDispatch({ 
+                query: "ADD_ONE_MORE", 
+                item: itemInCart })
+            }
+          >
+            +
+          </button>
+          <button
+            onClick={() =>
+              cartDispatch({ 
+                query: "REMOVE_FROM_CART", 
+                item: itemInCart })
+            }
+          >
+            Remove from Cart
+          </button>
+          <button
+            onClick={() =>
+              WishlistDispatch({
+                query: "ADD_OR_REMOVE_FROM_WISHLIST",
+                item: itemInCart
+              })
+            }
+          >
+            Wishlist
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+}
