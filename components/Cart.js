43e817a3fc65a4801c2f0eefbf93821b1913e51f
ardourApeX <joygupta501@gmail.com>
2021-04-07 13:14:50 +0530

    Test : Wiring Done for Cart Listing

--- components/Cart.js
+++ components/Cart.js
@@ -1,12 +1,26 @@
 import { useCart } from "../contexts/CartContext";
 import { useWishlist } from "../contexts/WishlistContext";
+import { useEffect, useState } from "react";
 
 export function Cart() {
-  const {cartState, cartDispatch} = useCart();
-  const {WishlistDispatch} = useWishlist();
+  const { cartState, cartDispatch } = useCart();
+  const { wishlistDispatch } = useWishlist();
+  const [balance, setBalance] = useState(0);
+  function useTotalPriceCalculator() {
+    useEffect(() => {
+      setBalance(
+        cartState.cartItems.reduce(
+          (total, item) => total + item.quantity * item.price,
+          0
+        )
+      );
+    });
+  }
+  useTotalPriceCalculator();
 
   return (
     <div>
+      Total {balance}
       {cartState.cartItems.map((itemInCart) => (
         <div id={itemInCart.id}>
           <img src={itemInCart.image} alt="img"></img>
@@ -14,9 +28,10 @@ export function Cart() {
           <strong>{itemInCart.price}</strong>
           <button
             onClick={() =>
-              cartDispatch({ 
-                query: "SUBTRACT_ONE_MORE", 
-                item: itemInCart })
+              cartDispatch({
+                query: "SUBTRACT_ONE_MORE",
+                item: itemInCart
+              })
             }
           >
             -
@@ -24,25 +39,27 @@ export function Cart() {
           {itemInCart.quantity}
           <button
             onClick={() =>
-              cartDispatch({ 
-                query: "ADD_ONE_MORE", 
-                item: itemInCart })
+              cartDispatch({
+                query: "ADD_ONE_MORE",
+                item: itemInCart
+              })
             }
           >
             +
           </button>
           <button
             onClick={() =>
-              cartDispatch({ 
-                query: "REMOVE_FROM_CART", 
-                item: itemInCart })
+              cartDispatch({
+                query: "REMOVE_FROM_CART",
+                item: itemInCart
+              })
             }
           >
             Remove from Cart
           </button>
           <button
             onClick={() =>
-              WishlistDispatch({
+              wishlistDispatch({
                 query: "ADD_OR_REMOVE_FROM_WISHLIST",
                 item: itemInCart
               })
