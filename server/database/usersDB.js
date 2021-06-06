dd0d46d36f0745cac1fe1f29e134f3d2d14e1eab
ardourApeX <joygupta501@gmail.com>
2021-06-07 01:43:36 +0530

    Feat : Added find by username/email functionality to server

--- server/database/usersDB.js
+++ server/database/usersDB.js
@@ -4,14 +4,29 @@ var users = [
 		name : "admin",
 		username : "admin",
 		email : "joygupta501@gmail.com",
-		password:"$2b$10$.kJKCYftXdOJn/QoutzrTuS6oXWlnj/NDuC4PIW8eWIHrHGxIrYjK"
+		password:"$2b$10$eOawiQIllq7OOCCL9.kmPOiFENtdRYKUDb.22HiOwvOiMlnG4fvu.",
+		wishlist: [],
+		cart : [],
 	},
 	{
 		id : 2,
 		name : "mock_user",
 		username : "mock_user",
 		email : "mockuser@tempmail.com",
-		password:"$2b$10$BaErhTKILaoCLHaJyY.jouRmWP.D1qYAPuByiDd8VFSCCrdPVzSeC"
+		password:"$2b$10$batsd5GzWWsJ96./L8oCDOaQvXIG/df/VKLe1eqQIIAhkZOq7Wviu",
+		wishlist: [],
+		cart : []
+	},
+	{
+		id: 3,
+  	name: 'testing',
+  	username: "testing@super",
+  	email: 'testing@gmail.com',
+  	password: '$2b$10$XvCFFQ6MOQLRUdzb2ZGXLOuASvD/8qkycG6q56i4lUWz.nB4g9TbO',
+  	question: 'What is your pet name?',
+  	answer: '$2b$10$HdIEcK8F9CNBTBrWLF4FgOdtxnGQQBLNA.ZnMpL/Y9bVhqPsnAhhy',
+  	wishlist: [],
+  	cart: []
 	}
 ]
 exports.users = users
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,20 +1,31 @@
-const express = require('express');
+const express = require("express");
 const bodyParser = require("body-parser");
-const jsonParser = bodyParser.json()
-const {
-    users
-} = require("../database/usersDB")
+const jsonParser = bodyParser.json();
+const { users } = require("../database/usersDB");
+const { findUserInDb } = require("../utilities");
 
 const userRoute = express.Router();
 userRoute.use(jsonParser);
-userRoute.get("/", function(request, response) {
-    setTimeout((() => {
-        response.status(200).send({
-            data: users
-        })
-    }), 2000)
-})
-userRoute.post("/:id", function(request, response) {
-    response.send(request.params.id)
-})
-exports.userRoute = userRoute
+userRoute.get("/", function (request, response) {
+	setTimeout(() => {
+		response.status(200).send({
+			data: users,
+		});
+	}, 2000);
+});
+userRoute.get("/validate", function (request, response) {
+	const data = request.body;
+	const userData = findUserInDb(data);
+	if (!userData) {
+		response
+			.status(404)
+			.send({ success: false, message: "User does not exist!" });
+	}
+	response
+		.status(200)
+		.send({
+			success: true,
+			data: { id: userData.id, question: userData.question },
+		});
+});
+exports.userRoute = userRoute;
