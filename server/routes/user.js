da9ff9bc6927593e3ecfac1f53d38337b47a0b0c
ardourApeX <joygupta501@gmail.com>
2021-06-11 03:43:18 +0530

    Feat : Added Change Password functionality to server

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json();
-const { users } = require("../database/usersDB");
+var { users } = require("../database/usersDB");
 const { findUserInDb, passwordValidation } = require("../utilities");
+const { encryption } = require("../utilities/encryption");
 
 const userRoute = express.Router();
 userRoute.use(jsonParser);
@@ -68,23 +69,50 @@ userRoute.post(
 					userData.answer
 				);
 				if (isPasswordSame.success) {
-					response
-						.status(isPasswordSame.result ? 200 : 401)
-						.send({
-							success: isPasswordSame.success,
-							result: isPasswordSame.result,
-						});
+					response.status(isPasswordSame.result ? 200 : 401).send({
+						success: isPasswordSame.success,
+						result: isPasswordSame.result,
+					});
 				} else {
-					response
-						.status(isPasswordSame.status)
-						.send({
-							success: isPasswordSame.success,
-							message: isPasswordSame.message,
-						});
+					response.status(isPasswordSame.status).send({
+						success: isPasswordSame.success,
+						message: isPasswordSame.message,
+					});
 				}
 			}
 		}
 	}
 );
+userRoute.post("/reset-password", async function (request, response) {
+	const data = request.body;
+	if (!data.password || !data.id) {
+		response
+			.status(400)
+			.send({ success: false, message: "Missing Parameters" });
+	} else {
+		const id = parseInt(data.id);
+		const userData = findUserInDb({ id });
+		if (!userData) {
+			response
+				.status(404)
+				.send({ success: false, message: "User does not exist!" });
+		} else {
+			const password = data.password;
+			const encryptedPassword = await encryption(password);
+			if (encryptedPassword.success) {
+				users = users.map((user) =>
+					user.id === id ? { ...user, password: encryptedPassword.hash } : user
+				);
+				response
+					.status(200)
+					.send({ success: true, message: "Password Updated Successfully" });
+			} else {
+				response
+					.status(500)
+					.send({ ...encryptedPassword, message: encryptedPassword.error });
+			}
+		}
+	}
+});
 
 exports.userRoute = userRoute;
