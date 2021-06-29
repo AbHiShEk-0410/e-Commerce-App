b77c33078f872c8286cf3d46facf3a7e9c226fc2
ardourApeX <joygupta501@gmail.com>
2021-06-29 23:16:04 +0530

    WIP : Added Grid layout to cart page

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -50,103 +50,112 @@ export default function Cart() {
 
 	return (
 		// For whole page
-		<div className="cart-listing">
-			{balance !== 0 &&
-				cartState.cartItems.map((itemInCart) => (
-					//For particular item
-					<div className="cart-tile" id={itemInCart.id}>
-						<div className="item-img">
-							<img src={itemInCart.img} alt="img"></img>
-						</div>
-						<div className="item-desc">
-							<h1>{itemInCart.name}</h1>
-							<div className="item-info">
-								<div className="food-info">
-									<img
-										src={
-											itemInCart.type === "vegetable"
-												? veg
-												: itemInCart.type === "non-veg"
-												? nonveg
-												: itemInCart.type === "Spices"
-												? veg
-												: egg
-										}
-										alt="food type"
-									/>
-									<FiBookmark
-										onClick={() =>
-											wishlistHandler(itemInCart, wishlistDispatch)
-										}
-										style={{
-											cursor: "pointer",
-											fontSize: "max(25px, 2vw)",
-											fill: checkItemInObject(
-												wishlistState.wishlistItems,
-												itemInCart
-											)
-												? "#0b002a"
-												: "white",
-										}}
-									/>
+		<div className="cart-page ">
+			<div className="cart-listing">
+				<div className="cart-items">
+					{balance !== 0 &&
+						cartState.cartItems.map((itemInCart) => (
+							//For particular item
+							<div className="cart-tile" id={itemInCart.id}>
+								<div className="item-img">
+									<img src={itemInCart.img} alt="img"></img>
 								</div>
-								<div>
-									{" "}
-									<span className="item-price">
-										<GiPayMoney
-											style={{
-												fontSize: "max(23px, 2vw)",
-												marginRight: "4px",
-												color: "var(--ternary-color)",
-												WebkitTransform :"scaleX(-1)"
-											}}
-										/>
-										<BiRupee
-											style={{
-												fontSize: "max(18px, 1.5vw)",
-												marginRight: "-4px",
-											}}
-										/>
-										<h3
-											style={{
-												fontSize: "max(23px, 2vw)",
-											}}
+								<div className="item-desc">
+									<h1>{itemInCart.name}</h1>
+									<div className="item-info">
+										<div className="food-info">
+											<img
+												src={
+													itemInCart.type === "vegetable"
+														? veg
+														: itemInCart.type === "non-veg"
+														? nonveg
+														: itemInCart.type === "Spices"
+														? veg
+														: egg
+												}
+												alt="food type"
+											/>
+											<FiBookmark
+												onClick={() =>
+													wishlistHandler(itemInCart, wishlistDispatch)
+												}
+												style={{
+													cursor: "pointer",
+													fontSize: "max(25px, 2vw)",
+													fill: checkItemInObject(
+														wishlistState.wishlistItems,
+														itemInCart
+													)
+														? "#0b002a"
+														: "white",
+												}}
+											/>
+										</div>
+										<div>
+											{" "}
+											<span className="item-price">
+												<GiPayMoney
+													style={{
+														fontSize: "max(23px, 2vw)",
+														marginRight: "4px",
+														color: "var(--ternary-color)",
+														WebkitTransform: "scaleX(-1)",
+													}}
+												/>
+												<BiRupee
+													style={{
+														fontSize: "max(18px, 1.5vw)",
+														marginRight: "-4px",
+													}}
+												/>
+												<h3
+													style={{
+														fontSize: "max(23px, 2vw)",
+													}}
+												>
+													{itemInCart.price * itemInCart.quantity}
+												</h3>
+											</span>
+										</div>
+									</div>
+
+									<div className="quantity-regulators">
+										<button
+											onClick={() =>
+												cartHandler(
+													"REMOVE_FROM_CART",
+													itemInCart,
+													cartDispatch
+												)
+											}
 										>
-											{itemInCart.price * itemInCart.quantity}
-										</h3>
-									</span>
+											-
+										</button>
+										<span>{itemInCart.quantity}</span>
+
+										<button
+											onClick={() =>
+												cartHandler("ADD_TO_CART", itemInCart, cartDispatch)
+											}
+										>
+											+
+										</button>
+									</div>
 								</div>
-							</div>
-
-							<div className="quantity-regulators">
-								<button
-									onClick={() =>
-										cartHandler("REMOVE_FROM_CART", itemInCart, cartDispatch)
-									}
-								>
-									-
-								</button>
-								<span>{itemInCart.quantity}</span>
-
-								<button
-									onClick={() =>
-										cartHandler("ADD_TO_CART", itemInCart, cartDispatch)
-									}
-								>
-									+
-								</button>
-							</div>
-						</div>
-						<div className="remove-from-cart">
-							<BsTrash
-								className="trash"
-								onClick={() =>
-									cartHandler("DELETE_FROM_CART", itemInCart, cartDispatch)
-								}
-							/>
-						</div>
-					</div> //Parent div
-				))}
+								<div className="remove-from-cart">
+									<BsTrash
+										className="trash"
+										onClick={() =>
+											cartHandler("DELETE_FROM_CART", itemInCart, cartDispatch)
+										}
+									/>
+								</div>
+							</div> //Parent div
+						))}
+				</div>
+				{balance !== 0 && <div className="billing">cvcv</div>}
+			</div>
 			{balance === 0 && <EmptyCart />}
 		</div>
 	);
